368a37d618029094a5fea76a3a8eb2a333e684bd
Ilya Chudin <chudin.ilya@gmail.com>
2020-04-07 14:15:30 +0400

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
-
+const imagemin = require("gulp-imagemin");
+ 
 function css() {
   return gulp
     .src("./src/css/*.css")
@@ -9,4 +10,13 @@ function css() {
     .pipe(gulp.dest("./css"));
 }
 
-exports.default = css;
+function img() {
+  return gulp
+    .src("./src/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./img"));
+}
+
+exports.css = css;
+exports.img = img;
+exports.default = gulp.parallel(css, img);

f042e7e504269a80898b8262c194bcb24e801aa5
Ilya Chudin <chudin.ilya@gmail.com>
2020-04-08 11:10:49 +0400

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const imagemin = require("gulp-imagemin");
- 
+const htmlmin = require("gulp-htmlmin");
+const terser = require("gulp-terser-js");
+
 function css() {
   return gulp
     .src("./src/css/*.css")
@@ -10,13 +12,42 @@ function css() {
     .pipe(gulp.dest("./css"));
 }
 
-function img() {
+function svg() {
   return gulp
-    .src("./src/img/*")
-    .pipe(imagemin())
+    .src("./src/img/*.svg")
+    .pipe(
+      imagemin([
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }],
+        }),
+      ])
+    )
     .pipe(gulp.dest("./img"));
 }
 
+function html() {
+  return gulp
+    .src("./src/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./"));
+}
+
+function js() {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(
+      terser({
+        mangle: {
+          toplevel: true,
+        },
+      })
+    )
+    .pipe(gulp.dest("./js/"));
+}
+
 exports.css = css;
-exports.img = img;
-exports.default = gulp.parallel(css, img);
+exports.svg = svg;
+exports.html = html;
+exports.js = js;
+exports.nosvg = gulp.parallel(html, css, js);
+exports.default = gulp.parallel(html, css, svg, js);

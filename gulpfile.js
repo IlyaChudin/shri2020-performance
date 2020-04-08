f6cbfaa94406bc8f2e9293a6740fadd251c58d3b
Ilya Chudin <chudin.ilya@gmail.com>
2020-04-08 19:45:11 +0400

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const cssnano = require("cssnano");
 const imagemin = require("gulp-imagemin");
 const htmlmin = require("gulp-htmlmin");
 const terser = require("gulp-terser-js");
+const inlinesource = require('gulp-inline-source');
 
 function css() {
   return gulp
@@ -44,13 +45,25 @@ function js() {
     )
     .pipe(gulp.dest("./js/"));
 }
+
+const nosvg = gulp.series(gulp.parallel(html, css, js), inline);
+
 function watch() {
-  gulp.watch("./src/**/*.*",gulp.parallel(html, css, js));
+  gulp.watch("./src/**/*.*", nosvg);
 }
+
+function inline() {
+  return gulp.src('./index.html')
+  .pipe(inlinesource())
+  .pipe(gulp.dest('./'));
+}
+
+
 exports.css = css;
 exports.svg = svg;
 exports.html = html;
 exports.js = js;
 exports.watch = watch;
-exports.nosvg = gulp.parallel(html, css, js);
-exports.default = gulp.parallel(html, css, svg, js);
+exports.inline = inline;
+exports.nosvg = nosvg;
+exports.default = gulp.parallel(nosvg, svg);
